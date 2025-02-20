132d27474fd195ee3055b5ab3877d60c9548f358
DenLadovir <denis.ladovir@yandex.ru>
2025-02-20 21:36:10 +0300

    changed DB connection logic

--- config/config.go
+++ config/config.go
@@ -1,8 +1,6 @@
 package config
 
 import (
-	"github.com/joho/godotenv"
-	"log"
 	"os"
 )
 
@@ -17,20 +15,14 @@ type Config struct {
 }
 
 func LoadConfig() *Config {
-	// Загружаем переменные из файла config.env
-	err := godotenv.Load("config.env")
-	if err != nil {
-		log.Fatalf("Error loading .env file: %v", err)
-	}
-
 	// Читаем переменные окружения
 	return &Config{
-		DBHost:     os.Getenv("DB_HOST"),
-		DBPort:     os.Getenv("DB_PORT"),
-		DBUser:     os.Getenv("DB_USER"),
-		DBPassword: os.Getenv("DB_PASSWORD"),
-		DBName:     os.Getenv("DB_NAME"),
-		DBSSLMode:  os.Getenv("DB_SSLMODE"),
+		DBHost:     os.Getenv("POSTGRES_DB_HOST"),
+		DBPort:     os.Getenv("POSTGRES_DB_PORT"),
+		DBUser:     os.Getenv("POSTGRES_USER"),
+		DBPassword: os.Getenv("POSTGRES_PASSWORD"),
+		DBName:     os.Getenv("POSTGRES_DB"),
+		DBSSLMode:  os.Getenv("POSTGRES_DB_SSLMODE"),
 		AppPort:    os.Getenv("APP_PORT"),
 	}
 }
--- main.go
+++ main.go
@@ -1,33 +1,21 @@
 package main
 
 import (
+	"Wallet/config"
 	"Wallet/database"
 	"Wallet/handlers"
 	"fmt"
 	"github.com/gorilla/mux"
-	"github.com/joho/godotenv"
 	"log"
 	"net/http"
-	"os"
 )
 
 func main() {
 	// Загружаем переменные окружения из config.env
-	err := godotenv.Load("config.env")
-	if err != nil {
-		log.Printf("Warning: Could not load config.env file, using environment variables instead")
-	}
-
-	// Получаем параметры из переменных окружения
-	dbHost := getEnv("DB_HOST", "localhost")
-	dbPort := getEnv("DB_PORT", "5432")
-	dbUser := getEnv("POSTGRES_USER", "user")             // Изменено на POSTGRES_USER
-	dbPassword := getEnv("POSTGRES_PASSWORD", "password") // Изменено на POSTGRES_PASSWORD
-	dbName := getEnv("POSTGRES_DB", "wallet_db")          // Изменено на POSTGRES_DB
-	appPort := getEnv("APP_PORT", "8080")
+	cfg := config.LoadConfig()
 
 	// Инициализируем базу данных
-	database.InitDB(dbHost, dbPort, dbUser, dbPassword, dbName)
+	database.InitDB(cfg.DBHost, cfg.DBPort, cfg.DBUser, cfg.DBPassword, cfg.DBName)
 
 	// Создаём маршрутизатор
 	r := mux.NewRouter()
@@ -39,15 +27,6 @@ func main() {
 	r.HandleFunc("/api/v1/wallets", handlers.GetAllWalletsHandler).Methods("GET")
 
 	// Запускаем сервер
-	fmt.Printf("Server is running. Port - %s...\n", appPort)
-	log.Fatal(http.ListenAndServe(":"+appPort, r))
-}
-
-// getEnv возвращает значение переменной окружения или значение по умолчанию
-func getEnv(key, defaultValue string) string {
-	value := os.Getenv(key)
-	if value == "" {
-		return defaultValue
-	}
-	return value
+	fmt.Printf("Server is running. Port - %s...\n", cfg.AppPort)
+	log.Fatal(http.ListenAndServe(":"+cfg.AppPort, r))
 }
